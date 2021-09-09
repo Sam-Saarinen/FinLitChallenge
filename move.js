eb1d252345e255b7c8d709569292afa38175cf3c
(S)am <sam@edapt.app>
2021-09-09 13:59:15 -0400

    Fresh build with corrected scripts.

--- move.js
+++ move.js
@@ -1,20 +1,9 @@
 const { exec } = require("child_process");
 
-// Have to use Windows move command instead of mv""
-exec("rmdir docs;", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+//This file can have race conditions unless each command is called in the callback of the previous.
 
 // Have to use Windows move command instead of mv""
-exec("move build docs;", (error, stdout, stderr) => {
+exec("if exist docs rmdir docs /s /q;", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -24,6 +13,18 @@ exec("move build docs;", (error, stdout, stderr) => {
         return;
     }
     console.log(`stdout: ${stdout}`);
+    // Have to use Windows move command instead of mv""
+    exec("move build docs;", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
 });
 
 module.exports = ()=>{}; //Seems necessary to make a weird module error go away.
