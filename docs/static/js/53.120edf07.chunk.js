db036cb3cc6526cbe6e22a48122df2ad8fae16d5
(S)am <sam@edapt.app>
2021-09-10 12:19:29 -0400

    Fresh Build

---
+++ docs/static/js/53.120edf07.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpFinLitChallenge=this.webpackJsonpFinLitChallenge||[]).push([[53],{136:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(15),o=e(13),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.120edf07.chunk.js.map
